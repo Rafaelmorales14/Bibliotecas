251c0632ca62bb98ac83b6ac9b3373ff7d686e14
Rafael Blasques Morales <rafaelblasques0611@gmail.com>
2026-04-23 20:28:55 -0300

    Create LivroController.java

---
+++ src/main/java/com/gestao/biblioteca/controllers/LivroController.java
@@ -0,0 +1,66 @@
+package com.gestao.biblioteca.controllers;
+
+import com.gestao.biblioteca.models.LivroModel;
+import com.gestao.biblioteca.services.LivroService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/livros")
+public class LivroController {
+
+    private final LivroService service;
+
+    public LivroController(LivroService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<LivroModel> post(@RequestBody LivroModel livro) {
+        LivroModel request = service.post(livro);
+        URI uri = ServletUriComponentsBuilder.
+                fromCurrentRequest().
+                path("/{id}").
+                buildAndExpand(request.getId()).
+                toUri();
+
+        return ResponseEntity.created(uri).body(request);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LivroModel>> getAll() {
+        List<LivroModel> livros = service.getAll();
+
+        return ResponseEntity.ok().body(livros);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LivroModel> getById(@PathVariable Long id) {
+        return service.getById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        if (service.getById(id).isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<LivroModel> put(@RequestBody LivroModel livro, @PathVariable Long id) {
+        if (service.getById(id).isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(service.put(livro, id));
+    }
+}
