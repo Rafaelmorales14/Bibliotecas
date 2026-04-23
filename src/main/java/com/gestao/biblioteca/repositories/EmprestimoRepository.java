1657632f932b8c9a44c106c758c22b7acc974ef3
Rafael Blasques Morales <rafaelblasques0611@gmail.com>
2026-04-22 22:04:01 -0300

    update repository and create loan service

--- src/main/java/com/gestao/biblioteca/repositories/EmprestimoRepository.java
+++ src/main/java/com/gestao/biblioteca/repositories/EmprestimoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmprestimoRepository extends JpaRepository<Long, EmprestimoModel> {
+public interface EmprestimoRepository extends JpaRepository<EmprestimoModel, Long> {
 }
---
+++ src/main/java/com/gestao/biblioteca/services/EmprestimoService.java
@@ -0,0 +1,46 @@
+package com.gestao.biblioteca.services;
+
+import com.gestao.biblioteca.models.EmprestimoModel;
+import com.gestao.biblioteca.repositories.EmprestimoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmprestimoService {
+
+    private final EmprestimoRepository repository;
+
+    public EmprestimoService(EmprestimoRepository repository) {
+        this.repository = repository;
+    }
+
+    public EmprestimoModel post(EmprestimoModel emprestimo) {
+        return repository.save(emprestimo);
+    }
+
+    public List<EmprestimoModel> getAll(){
+        return repository.findAll();
+    }
+
+    public Optional<EmprestimoModel> getById(Long id) {
+        return repository.findById(id);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public EmprestimoModel put(EmprestimoModel emprestimo, Long id) {
+        EmprestimoModel emprestimoExistente = repository.
+                findById(id).
+                orElseThrow(()-> new RuntimeException("Emprestimo não encontrado"));
+
+        emprestimoExistente.setUsuario(emprestimo.getUsuario());
+        emprestimoExistente.setLivro(emprestimo.getLivro());
+        emprestimoExistente.setStatus(emprestimo.getStatus());
+
+        return repository.save(emprestimoExistente);
+    }
+}
